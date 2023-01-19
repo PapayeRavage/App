d6a5e2a08a596b010a05e8544d7218545dfe8a0c
Thibaut ROUTHE <e2104605@ensimlan.univ-lemans.fr>
2023-01-19 09:55:34 +0100

    Initial commit

---
+++ src/main/java/fr/ensim/app/model/Address.java
@@ -0,0 +1,39 @@
+package fr.ensim.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date creation;
+    private String content;
+    public String getContent() {
+        return content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreation() {
+        return creation;
+    }
+
+    public void setCreation(Date creation) {
+        this.creation = creation;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
